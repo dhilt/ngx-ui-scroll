ba03d82b416c27bc45e083f3dfac0af6631d436c
NA <d.alexanov@gmail.com>
2017-09-29 04:53:49 +0300

    Angular 4

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { Ng2UiScrollAppPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('ng2-ui-scroll-app App', () => {
-  let page: Ng2UiScrollAppPage;
+describe('project-name App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new Ng2UiScrollAppPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });

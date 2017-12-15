c7a41e92a862ec9a9e40ee32eda1cbc50a997a35
khajjit <yuvzhenko@inbox.ru>
2017-12-15 21:23:44 +0800

    Creative chaos (1)

--- src/app/ui-scroll-directive/ui-scroll.component.ts
+++ src/app/ui-scroll-directive/ui-scroll.component.ts
@@ -2,74 +2,52 @@ import {Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, OnDestroy
 import {ContentChild, TemplateRef, ElementRef, Renderer2} from '@angular/core';
 import {HostListener} from '@angular/core';
 
-import { UiScrollService } from './ui-scroll.service';
+import {UiScrollService} from './ui-scroll.service';
 
+import {AsyncSubject} from 'rxjs/AsyncSubject';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
-import {AsyncSubject} from 'rxjs/AsyncSubject';
 
+import debouncedRound from './modules/debouncedRound';
+import Direction from './modules/direction';
 import Workflow from './modules/workflow';
 import Elements from './modules/elements';
 import Data from './modules/data';
-import Direction from './modules/direction';
-
-import debouncedRound from './modules/debouncedRound';
-
-@Component({
-  selector: 'ui-scroll-body',
-  template: `
-    <h2>{{title}} {{datasource}} </h2>
-    <ng-content></ng-content>
-  `
-})
-export class UiScrollComponent implements OnInit {
-  private title = 'Hello from ui-scroll body';
-  private datasource;
-
-   constructor(
-     private uiScrollService: UiScrollService
-   ) {
-   }
-
-  ngOnInit() {
-    this.datasource = this.uiScrollService.getDatasource();
-  }
-}
-
-/*
-
-
 
 @Component({
   selector: 'ui-scroll',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './ui-scroll.component.html'
 })
-
 export class UiScrollComponent implements OnInit, OnDestroy {
 
-  @Input() datasource;
   @ContentChild(TemplateRef) templateVariable: TemplateRef<any>;
+  private onScrollListener: Function;
+  private datasource;
 
-  onScrollListener: Function;
-
-  constructor(private changeDetector: ChangeDetectorRef, private elementRef: ElementRef, private renderer: Renderer2) {
-  }
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+    private uiScrollService: UiScrollService,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit() {
+    this.datasource = this.uiScrollService.getDatasource();
     Elements.initialize(this.elementRef);
     Data.initialize(this);
     Workflow.initialize(this);
     this.onScrollListener = this.renderer.listen(Elements.viewport, 'scroll', (event) =>
       debouncedRound(() => Workflow.run(event), 25)
     );
-    Workflow.run(Direction.bottom);
-    Workflow.run(Direction.top);
+    // Workflow.run(Direction.bottom);
+    // Workflow.run(Direction.top);
+    console.log(this);
+    console.log(this.elementRef.nativeElement.innerHTML.trim());
+    console.log(this.templateVariable);  // Here should be info from app.component.html..?
   }
 
   ngOnDestroy() {
     this.onScrollListener();
   }
-
 }
-*/

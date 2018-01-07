4cfd359f6d6508c600e6507bdfa49bd1b0225bad
dhilt <d.alexanov@gmail.com>
2018-01-07 21:06:24 +0300

    src/ngx-ui-scroll fix

--- app/app/app.module.ts
+++ app/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UiScrollModule } from '../../src';
+import { UiScrollModule } from '../../src/ngx-ui-scroll';
 
 @NgModule({
   declarations: [AppComponent],

6b909848e0bd6f348e82a0b702aa7bb25bd0d714
BitDen <yuvzhenko@inbox.ru>
2018-01-19 11:36:35 +0800

    Import directive through UiScrollModule

--- tests/ngx-ui-scroll.component.spec.ts
+++ tests/ngx-ui-scroll.component.spec.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectorRef, ElementRef, ViewContainerRef, DebugElement } from '@angular/core';
 import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
 
-import { UiScrollDirective } from '../src/directive/ui-scroll.directive';
+import { UiScrollModule } from '../src/module/ui-scroll.module';
 import { TestedComponent } from './testedComponent';
 
 describe('An UiScroll directive', () => {
@@ -10,14 +10,20 @@ describe('An UiScroll directive', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TestedComponent, UiScrollDirective]
+      imports: [UiScrollModule],
+      declarations: [TestedComponent]
     });
     fixture = TestBed.createComponent(TestedComponent);
     component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture.detectChanges();
   });
 
   it('should have an instance', () => {
     expect(component).toBeDefined();
+  });
+
+  it('should correctly be called html-tag with directive', () => {
+    const el = fixture.debugElement.children[0];
+    expect(el.name).toBe('app-ui-scroll');
   });
 });

695feab018c7072379d1d303fc534c240fa7ae58
khajjit <yuvzhenko@inbox.ru>
2018-01-22 15:55:35 +0800

    Added tests on markup

--- tests/ngx-ui-scroll.component.spec.ts
+++ tests/ngx-ui-scroll.component.spec.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectorRef, ElementRef, ViewContainerRef, DebugElement } from '@angular/core';
 import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { UiScrollModule } from '../src/module/ui-scroll.module';
 import { TestedComponent } from './testedComponent';
@@ -20,10 +21,31 @@ describe('An UiScroll directive', () => {
 
   it('should have an instance', () => {
     expect(component).toBeDefined();
+    expect(component.datasource).toBeDefined(Object);
+    expect(component.datasource.get).toBeDefined(Function);
   });
 
   it('should correctly be called html-tag with directive', () => {
-    const el = fixture.debugElement.children[0];
-    expect(el.name).toBe('app-ui-scroll');
+    expect(fixture.debugElement.children[0].name).toBe('app-ui-scroll');
+  });
+
+  const getElemByAttr = (attr) => fixture.debugElement.query(By.css(attr));
+
+  it('should create tags: data-padding-top and data-padding-bottom', () => {
+    expect(getElemByAttr('[data-padding-top]')).not.toBeNull();
+    expect(getElemByAttr('[data-padding-bottom]')).not.toBeNull();
+
+    // expect(getElemByAttr('[data-padding-top]')).nativeElement.style.height).toBe('36px');
+    // expect(getElemByAttr('[data-padding-bottom]')).nativeElement.style.height).toBe('0px');
+  });
+
+  it('should contain some div with items inside', () => {
+    for (let j = 86; j <= 99; j++) {  // The numbers need to be changed
+      const elem = getElemByAttr(`[id="i-0-${j}"]`);
+      expect(elem.name).toBe('div');
+      expect(elem.nativeElement.textContent).toMatch(`${j} : item #${j}`);
+      // expect(elem.childNodes[1].styles.position).toBeUndefined();
+      // expect(elem.childNodes[1].styles.left).not.toBe('-9999px');
+    }
   });
 });

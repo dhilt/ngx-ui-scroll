9c5673b6f5728c4f4bc158f3cedd2a7ff4ba8883
khajjit <yuvzhenko@inbox.ru>
2018-01-24 20:30:08 +0800

    Trying fix karma start

--- tests/ngx-ui-scroll.component.spec.ts
+++ tests/ngx-ui-scroll.component.spec.ts
@@ -1,5 +1,5 @@
+import { TestBed, ComponentFixture, ComponentFixtureAutoDetect, inject, async } from '@angular/core/testing';
 import { ChangeDetectorRef, ElementRef, ViewContainerRef, DebugElement } from '@angular/core';
-import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { UiScrollModule } from '../src/module/ui-scroll.module';
@@ -7,17 +7,21 @@ import { TestedComponent } from './testedComponent';
 
 describe('An UiScroll directive', () => {
   let component: TestedComponent;
+  // let nativeComponent;
   let fixture: ComponentFixture <TestedComponent>;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [UiScrollModule],
-      declarations: [TestedComponent]
+      declarations: [TestedComponent],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     });
     fixture = TestBed.createComponent(TestedComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    component = fixture.componentInstance;  // access properties and methods
+    // nativeComponent = fixture.nativeElement;  // access DOM
+  }));
 
   it('should have an instance', () => {
     expect(component).toBeDefined();
@@ -25,27 +29,33 @@ describe('An UiScroll directive', () => {
     expect(component.datasource.get).toEqual(jasmine.any(Function));
   });
 
-  it('should correctly be called html-tag with directive', () => {
-    expect(fixture.debugElement.children[0].name).toBe('app-ui-scroll');
-  });
-
   const getElemByAttr = (attr) => fixture.debugElement.query(By.css(attr));
 
-  it('should create tags: data-padding-top and data-padding-bottom', () => {
-    expect(getElemByAttr('[data-padding-top]')).not.toBeNull();
-    expect(getElemByAttr('[data-padding-bottom]')).not.toBeNull();
-
-    // expect(getElemByAttr('[data-padding-top]')).nativeElement.style.height).toBe('36px');
-    // expect(getElemByAttr('[data-padding-bottom]')).nativeElement.style.height).toBe('0px');
+  it('should correctly be called html-tag with directive', () => {
+    expect(getElemByAttr('app-ui-scroll')).toBeDefined();  // should be renamed into ui-scroll
   });
 
-  it('should contain some div with items inside', () => {
-    for (let j = 86; j <= 99; j++) {  // The numbers need to be changed
-      const elem = getElemByAttr(`[id="i-0-${j}"]`);
-      expect(elem.name).toBe('div');
-      expect(elem.nativeElement.textContent).toMatch(`${j} : item #${j}`);
-      // expect(elem.childNodes[1].styles.position).toBeUndefined();
-      // expect(elem.childNodes[1].styles.left).not.toBe('-9999px');
+  it('should create tags: data-padding-top and data-padding-bottom', () => {
+    expect(getElemByAttr('[data-padding-top]')).toBeDefined();
+    expect(getElemByAttr('[data-padding-bottom]')).toBeDefined();
+
+    // expect(getElemByAttr('[data-padding-top]').nativeElement.style.height).toEqual('36px');
+    expect(getElemByAttr('[data-padding-bottom]').nativeElement.style.height).toEqual('0px');
+  });
+
+  it('should contain only items with id from 86 up to 99', () => {
+    const firstId = 86;
+    const lastId = 99;
+    for (let id = firstId; id <= lastId; id++) {
+      const elem = getElemByAttr(`[id="i-0-${id}"]`);
+      expect(elem.name).toEqual('div');
+      expect(elem.nativeElement.textContent).toMatch(`${id} : item #${id}`);
+      expect(elem.childNodes[1].styles.position).toBeNull();
+      expect(elem.childNodes[1].styles.left).toBeNull();
     }
+
+    expect(getElemByAttr(`[id="i-0-${firstId - 1}"]`)).toBeNull();
+    /* This comparison in case of unsuccessful outcome leads to a hangup. */
+    // expect(getElemByAttr(`[id="i-0-${lastId + 1}"]`)).toBeNull();
   });
 });

f49818a02279b227acf746da6c18ec74a3dc8019
dhilt <d.alexanov@gmail.com>
2018-02-09 22:09:49 +0300

    Settings interface

--- src/component/classes/settings.ts
+++ src/component/classes/settings.ts
@@ -1,11 +1,14 @@
+import { Settings as SettingsInterface } from '../interfaces/settings';
+
 export class Settings {
   startIndex = 1;
   bufferSize = 5;
   padding = 0.5; // of viewport height
 
+  // internal settings
   itemIdPrefix = 'ui-scroll-0-'; // todo : scroll instance index ?
 
-  constructor(settings?: Settings) {
+  constructor(settings?: SettingsInterface) {
     if (settings) {
       Object.assign(this, settings);
     }
---
+++ src/component/interfaces/settings.ts
@@ -0,0 +1,5 @@
+export interface Settings {
+  startIndex: number;
+  bufferSize: number;
+  padding: number;
+}

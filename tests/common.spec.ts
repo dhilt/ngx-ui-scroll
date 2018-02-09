2fe644a68d82ac6fec98889c0e4e9c01bc58fba5
khajjit <yuvzhenko@inbox.ru>
2018-02-10 05:35:59 +0800

    Fixed test scrolling

--- tests/common.spec.ts
+++ tests/common.spec.ts
@@ -56,58 +56,58 @@ describe('Common tests', () => {
     expect(getElemByAttr(`[id="i-0-${lastId + 1}"]`)).toBeNull();
   }));
 
-  // it('should simulate scroll to top, and contain items with id from 81 up to 95', fakeAsync(() => {
-  //   let uiScroll = getElemByAttr('app-ui-scroll');
-  //   let eventListener = uiScroll.listeners.find(e => e.name === 'scroll');
-  //   uiScroll.nativeElement.scrollTop = 0;
-  //   eventListener.callback('top');  // ??? backward
-  //
-  //   fixture.whenStable().then(() => {
-  //     fixture.detectChanges();
-  //     tick(25);
-  //     fixture.detectChanges();
-  //
-  //     const firstId = 81;
-  //     const lastId = 95;
-  //     for (let id = firstId; id <= lastId; id++) {
-  //       const elem = getElemByAttr(`[id="i-0-${id}"]`);
-  //       expect(elem.name).toEqual('div');
-  //       expect(elem.nativeElement.textContent).toMatch(`${id} : item #${id}`);
-  //       expect(elem.childNodes[1].styles.position).toBeNull();
-  //       expect(elem.childNodes[1].styles.left).toBeNull();
-  //     }
-  //
-  //     expect(getElemByAttr('[data-padding-backward]').nativeElement.style.height).toEqual('0px');
-  //     expect(getElemByAttr('[data-padding-forward]').nativeElement.style.height).toEqual('72px');
-  //     expect(getElemByAttr(`[id="i-0-${firstId - 1}"]`)).toBeNull();
-  //     expect(getElemByAttr(`[id="i-0-${lastId + 1}"]`)).toBeNull();
-  //   });
-  // }));
+  it('should simulate scroll to top, and contain items with id from ??? up to ???', fakeAsync(() => {
+    const uiScroll = getElemByAttr('ui-scroll').parent;
+    const eventListener = uiScroll.listeners.find(e => e.name === 'scroll');
+    uiScroll.nativeElement.scrollTop = 0;
+    eventListener.callback('top');
 
-  // it('should simulate scroll to bottom, and contain items with id from 90 up to 104', fakeAsync(() => {
-  //   let uiScroll = getElemByAttr('app-ui-scroll');
-  //   let eventListener = uiScroll.listeners.find(e => e.name === 'scroll');
-  //   uiScroll.nativeElement.scrollTop = uiScroll.nativeElement.scrollHeight - uiScroll.nativeElement.clientHeight;
-  //   eventListener.callback('bottom');  // ??? forward
-  //
-  //   fixture.whenStable().then(() => {
-  //     fixture.detectChanges();
-  //     tick(25);
-  //     fixture.detectChanges();
-  //
-  //     const firstId = 90;
-  //     const lastId = 104;
-  //     for (let id = firstId; id <= lastId; id++) {
-  //       const elem = getElemByAttr(`[id="i-0-${id}"]`);
-  //       expect(elem.name).toEqual('div');
-  //       expect(elem.nativeElement.textContent).toMatch(`${id} : item #${id}`);
-  //       expect(elem.childNodes[1].styles.position).toBeNull();
-  //       expect(elem.childNodes[1].styles.left).toBeNull();
-  //     }
-  //     expect(getElemByAttr('[data-padding-backward]').nativeElement.style.height).toEqual('90px');
-  //     expect(getElemByAttr('[data-padding-forward]').nativeElement.style.height).toEqual('0px');
-  //     expect(getElemByAttr(`[id="i-0-${firstId - 1}"]`)).toBeNull();
-  //     expect(getElemByAttr(`[id="i-0-${lastId + 1}"]`)).toBeNull();
-  //   });
-  // }));
+    // fixture.whenStable().then(() => {
+    //   fixture.detectChanges();
+    //   tick(25);
+    //   fixture.detectChanges();
+    //
+    //   const firstId = 81;
+    //   const lastId = 95;
+    //   for (let id = firstId; id <= lastId; id++) {
+    //     const elem = getElemByAttr(`[id="i-0-${id}"]`);
+    //     expect(elem.name).toEqual('div');
+    //     expect(elem.nativeElement.textContent).toMatch(`${id} : item #${id}`);
+    //     expect(elem.childNodes[1].styles.position).toBeNull();
+    //     expect(elem.childNodes[1].styles.left).toBeNull();
+    //   }
+    //
+    //   expect(getElemByAttr('[data-padding-backward]').nativeElement.style.height).toEqual('0px');
+    //   expect(getElemByAttr('[data-padding-forward]').nativeElement.style.height).toEqual('72px');
+    //   expect(getElemByAttr(`[id="i-0-${firstId - 1}"]`)).toBeNull();
+    //   expect(getElemByAttr(`[id="i-0-${lastId + 1}"]`)).toBeNull();
+    // });
+  }));
+
+  it('should simulate scroll to bottom, and contain items with id from ??? up to ???', fakeAsync(() => {
+    const uiScroll = getElemByAttr('ui-scroll').parent;
+    const eventListener = uiScroll.listeners.find(e => e.name === 'scroll');
+    uiScroll.nativeElement.scrollTop = uiScroll.nativeElement.scrollHeight - uiScroll.nativeElement.clientHeight;
+    eventListener.callback('bottom');
+
+    // fixture.whenStable().then(() => {
+    //   fixture.detectChanges();
+    //   tick();
+    //   fixture.detectChanges();
+    //
+    //   const firstId = 90;
+    //   const lastId = 104;
+    //   for (let id = firstId; id <= lastId; id++) {
+    //     const elem = getElemByAttr(`[id="i-0-${id}"]`);
+    //     expect(elem.name).toEqual('div');
+    //     expect(elem.nativeElement.textContent).toMatch(`${id} : item #${id}`);
+    //     expect(elem.childNodes[1].styles.position).toBeNull();
+    //     expect(elem.childNodes[1].styles.left).toBeNull();
+    //   }
+    //   expect(getElemByAttr('[data-padding-backward]').nativeElement.style.height).toEqual('90px');
+    //   expect(getElemByAttr('[data-padding-forward]').nativeElement.style.height).toEqual('0px');
+    //   expect(getElemByAttr(`[id="i-0-${firstId - 1}"]`)).toBeNull();
+    //   expect(getElemByAttr(`[id="i-0-${lastId + 1}"]`)).toBeNull();
+    // });
+  }));
 });

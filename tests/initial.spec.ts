7f81c3c0e7335c41f1b9cbf28d725e12e133aa96
dhilt <d.alexanov@gmail.com>
2018-02-11 02:24:10 +0300

    calculate fetch data dynamically

--- tests/initial.spec.ts
+++ tests/initial.spec.ts
@@ -1,37 +1,52 @@
 import { async } from '@angular/core/testing';
 
 import { configureTestBed } from './scaffolding/testBed';
-import { generateTemplate } from './scaffolding/templates';
-import { InitialDatasource } from './scaffolding/datasources/initial';
+import { generateTemplate, TemplateSettings } from './scaffolding/templates';
 import { Direction } from '../src/component/interfaces/direction';
 import { Misc } from './miscellaneous/misc';
+import { Datasource } from '../src/component/interfaces/datasource';
+import { datasourceStore } from './scaffolding/datasources';
+
+const itemHeight = 20;
 
 describe('Initial spec', () => {
+  let datasource: Datasource;
+  let templateData;
   let misc: Misc;
-  const templateData = generateTemplate({
-    viewportHeight: 120
-  });
 
   beforeEach(async(() => {
-    const fixture = configureTestBed(InitialDatasource, templateData.template);
+    const fixture = configureTestBed(datasource, templateData.template);
     misc = new Misc(fixture);
   }));
 
-  it('should fetch 3 packs with no clip', () => {
-    expect(misc.workflow.fetchCount).toEqual(3);
-    expect(misc.padding[Direction.backward].getSize()).toEqual(0);
-    expect(misc.padding[Direction.forward].getSize()).toEqual(0);
+  const makeTest = (datasourceName: string, templateSettings: TemplateSettings) => {
+    datasource = datasourceStore[datasourceName];
+    templateData = generateTemplate(templateSettings);
 
-    const flowSettings = misc.workflow.settings;
-    const first = flowSettings.startIndex - flowSettings.bufferSize;
-    const last = flowSettings.startIndex + flowSettings.bufferSize * 2 - 1;
-    for (let index = first; index <= last; index++) {
-      const elem = misc.getItemElement(index);
-      expect(elem.name).toEqual('div');
-      expect(elem.nativeElement.innerText.trim()).toEqual(`${index} : item #${index}`);
-    }
-    expect(misc.getItemElement(first - 1)).toBeFalsy();
-    expect(misc.getItemElement(last + 1)).toBeFalsy();
-  });
+    const backwardLimit = templateSettings.viewportHeight * datasource.settings.padding;
+    const forwardLimit = templateSettings.viewportHeight + backwardLimit;
+    const backwardFetchCount = Math.ceil((backwardLimit / itemHeight) / datasource.settings.bufferSize);
+    const forwardFetchCount = Math.ceil((forwardLimit / itemHeight) / datasource.settings.bufferSize);
+    const fetchCount = backwardFetchCount + forwardFetchCount;
+
+    it(`should fetch ${fetchCount} packs with no clip`, () => {
+      expect(misc.workflow.fetchCount).toEqual(fetchCount);
+      expect(misc.padding[Direction.backward].getSize()).toEqual(0);
+      expect(misc.padding[Direction.forward].getSize()).toEqual(0);
+
+      const flowSettings = misc.workflow.settings;
+      const first = flowSettings.startIndex - backwardFetchCount * flowSettings.bufferSize;
+      const last = flowSettings.startIndex + forwardFetchCount * flowSettings.bufferSize - 1;
+      for (let index = first; index <= last; index++) {
+        const elem = misc.getItemElement(index);
+        expect(elem).toBeTruthy();
+        expect(elem.nativeElement.innerText.trim()).toEqual(`${index} : item #${index}`);
+      }
+      expect(misc.getItemElement(first - 1)).toBeFalsy();
+      expect(misc.getItemElement(last + 1)).toBeFalsy();
+    });
+  };
+
+  makeTest('initial', { viewportHeight: 200 });
 
 });

a1e6db0ae3d80d851c99a436ad62170ee3360687
Alexander Solovyev <supromikali@yandex.ru>
2018-02-15 14:36:38 +0400

    Initial spec: add "infinite" case

--- tests/initial-load.spec.ts
+++ tests/initial-load.spec.ts
@@ -9,48 +9,75 @@ const generateMetaTitle = (settings): string =>
 `buffer size = ${settings.datasourceSettings.bufferSize}, ` +
 `padding = ${settings.datasourceSettings.padding}`;
 
+const configList = [{
+  datasourceSettings: { startIndex: 1, bufferSize: 1, padding: 2 },
+  templateSettings: { viewportHeight: 20 }
+}, {
+  datasourceSettings: { startIndex: 1, bufferSize: 3, padding: 0.5 },
+  templateSettings: { viewportHeight: 120 }
+}, {
+  datasourceSettings: { startIndex: -99, bufferSize: 5, padding: 0.5 },
+  templateSettings: { viewportHeight: 200 }
+}];
+
+const configListWithClip = [{
+  datasourceSettings: { startIndex: 1, bufferSize: 2, padding: 1 },
+  templateSettings: { viewportHeight: 20 }
+}, {
+  datasourceSettings: { startIndex: 1, bufferSize: 3, padding: 33 },
+  templateSettings: { viewportHeight: 70 }
+}, {
+  datasourceSettings: { startIndex: 33, bufferSize: 4, padding: 2 },
+  templateSettings: { viewportHeight: 99 }
+}, {
+  datasourceSettings: { startIndex: -50, bufferSize: 10, padding: 0.3 },
+  templateSettings: { viewportHeight: 400 }
+}];
+
+const configListInfinite = [ ...configList, ...configListWithClip ]
+  .map(i => ({ ...i, datasourceSettings: { ...i.datasourceSettings, infinite: true }}));
+
 describe('Initial Load Spec', () => {
 
+  const testViewport = (settings) => (misc) => (done) => {
+    const startIndex = settings.datasourceSettings.startIndex;
+    const bufferSize = settings.datasourceSettings.bufferSize;
+    const padding = settings.datasourceSettings.padding;
+    const viewportHeight = settings.templateSettings.viewportHeight;
+
+    const backwardLimit = viewportHeight * padding;
+    const forwardLimit = viewportHeight + backwardLimit;
+    const backwardFetchCount = Math.ceil((backwardLimit / itemHeight) / bufferSize);
+    const forwardFetchCount = Math.ceil((forwardLimit / itemHeight) / bufferSize);
+    const fetchCount = backwardFetchCount + forwardFetchCount;
+    const first = startIndex - backwardFetchCount * bufferSize;
+    const last = startIndex + forwardFetchCount * bufferSize - 1;
+
+    expect(misc.workflow.fetchCount).toEqual(fetchCount);
+    expect(misc.workflow.buffer.items.length).toEqual(last - first + 1);
+    expect(misc.padding[Direction.backward].getSize()).toEqual(0);
+    expect(misc.padding[Direction.forward].getSize()).toEqual(0);
+    expect(misc.getElementText(first)).toEqual(`${first} : item #${first}`);
+    expect(misc.getElementText(last)).toEqual(`${last} : item #${last}`);
+    expect(misc.getElement(first - 1)).toBeFalsy();
+    expect(misc.getElement(last + 1)).toBeFalsy();
+
+    done();
+  };
+
   describe('No Clip', () => {
-
-    const testViewport = (settings) => (misc) => (done) => {
-      const startIndex = settings.datasourceSettings.startIndex;
-      const bufferSize = settings.datasourceSettings.bufferSize;
-      const padding = settings.datasourceSettings.padding;
-      const viewportHeight = settings.templateSettings.viewportHeight;
-
-      const backwardLimit = viewportHeight * padding;
-      const forwardLimit = viewportHeight + backwardLimit;
-      const backwardFetchCount = Math.ceil((backwardLimit / itemHeight) / bufferSize);
-      const forwardFetchCount = Math.ceil((forwardLimit / itemHeight) / bufferSize);
-      const fetchCount = backwardFetchCount + forwardFetchCount;
-      const first = startIndex - backwardFetchCount * bufferSize;
-      const last = startIndex + forwardFetchCount * bufferSize - 1;
-
-      expect(misc.workflow.fetchCount).toEqual(fetchCount);
-      expect(misc.workflow.buffer.items.length).toEqual(last - first + 1);
-      expect(misc.padding[Direction.backward].getSize()).toEqual(0);
-      expect(misc.padding[Direction.forward].getSize()).toEqual(0);
-      expect(misc.getElementText(first)).toEqual(`${first} : item #${first}`);
-      expect(misc.getElementText(last)).toEqual(`${last} : item #${last}`);
-      expect(misc.getElement(first - 1)).toBeFalsy();
-      expect(misc.getElement(last + 1)).toBeFalsy();
-
-      done();
-    };
-
-    const configList = [{
-      datasourceSettings: { startIndex: 1, bufferSize: 1, padding: 2 },
-      templateSettings: { viewportHeight: 20 }
-    }, {
-      datasourceSettings: { startIndex: 1, bufferSize: 3, padding: 0.5 },
-      templateSettings: { viewportHeight: 120 }
-    }, {
-      datasourceSettings: { startIndex: -99, bufferSize: 5, padding: 0.5 },
-      templateSettings: { viewportHeight: 200 }
-    }];
-
     configList.forEach(config =>
+      makeTest({
+        metatitle: generateMetaTitle(config),
+        title: 'should fetch some items with no clip',
+        config: config,
+        it: testViewport(config)
+      })
+    );
+  });
+
+  describe('No Clip (infinite)', () => {
+    configListInfinite.forEach(config =>
       makeTest({
         metatitle: generateMetaTitle(config),
         title: 'should fetch some items with no clip',
@@ -63,7 +90,7 @@ describe('Initial Load Spec', () => {
 
   describe('Clip', () => {
 
-    const testViewport = (settings) => (misc) => (done) => {
+    const testViewportWithClip = (settings) => (misc) => (done) => {
       const startIndex = settings.datasourceSettings.startIndex;
       const bufferSize = settings.datasourceSettings.bufferSize;
       const padding = settings.datasourceSettings.padding;
@@ -100,26 +127,12 @@ describe('Initial Load Spec', () => {
       done();
     };
 
-    const configListWithClip = [{
-      datasourceSettings: { startIndex: 1, bufferSize: 2, padding: 1 },
-      templateSettings: { viewportHeight: 20 }
-    }, {
-      datasourceSettings: { startIndex: 1, bufferSize: 3, padding: 33 },
-      templateSettings: { viewportHeight: 70 }
-    }, {
-      datasourceSettings: { startIndex: 33, bufferSize: 4, padding: 2 },
-      templateSettings: { viewportHeight: 99 }
-    }, {
-      datasourceSettings: { startIndex: -50, bufferSize: 10, padding: 0.3 },
-      templateSettings: { viewportHeight: 400 }
-    }];
-
     configListWithClip.forEach(config =>
       makeTest({
         metatitle: generateMetaTitle(config),
         title: 'should fetch some items with clip',
         config,
-        it: testViewport(config)
+        it: testViewportWithClip(config)
       })
     );
   });

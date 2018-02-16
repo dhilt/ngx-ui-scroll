c9dde49c3310076673dcfb6532864550243238a5
dhilt <d.alexanov@gmail.com>
2018-02-16 12:39:06 +0300

    different datasource.get implementations (demo)

--- app/app/app.component.ts
+++ app/app/app.component.ts
@@ -4,6 +4,48 @@ import { Observable } from 'rxjs/Observable';
 import { Datasource } from '../../public_api';
 //import { Datasource } from 'ngx-ui-scroll';
 
+const datasourceGet = {
+  'callback': (index: number, count: number, success) => {
+    console.log('requested index = ' + index + ', count = ' + count);
+    setTimeout(() => {
+      let data = [];
+      for (let i = index; i <= index + count - 1; i++) {
+        data.push({
+          id: i,
+          text: "item #" + i
+        });
+      }
+      success(data);
+    }, 100);
+  },
+  'observable': (index: number, count: number) => Observable.create(observer => {
+    console.log('requested index = ' + index + ', count = ' + count);
+    setTimeout(() => {
+      let data = [];
+      for (let i = index; i <= index + count - 1; i++) {
+        data.push({
+          id: i,
+          text: "item #" + i
+        });
+      }
+      observer.next(data);
+    }, 100);
+  }),
+  'promise': (index: number, count: number) => new Promise((resolve, reject) => {
+    console.log('requested index = ' + index + ', count = ' + count);
+    setTimeout(() => {
+      let data = [];
+      for (let i = index; i <= index + count - 1; i++) {
+        data.push({
+          id: i,
+          text: "item #" + i
+        });
+      }
+      resolve(data);
+    }, 100);
+  })
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,20 +55,7 @@ export class AppComponent {
   public title = 'app works!';
 
   public datasource: Datasource = {
-    get: (index: number, count: number) => Observable.create(observer => {
-      console.log('requested index = ' + index + ', count = ' + count);
-      setTimeout(() => {
-        let data = [];
-        for (let i = index; i <= index + count - 1; i++) {
-          data.push({
-            id: i,
-            text: "item #" + i
-          });
-        }
-        observer.next(data);
-      }, 100);
-    }),
-
+    get: datasourceGet['callback'],
     settings: {
       bufferSize: 5
     }

6361495b035d3b83e11687a70a18695f3508a1b2
d.alexanov <d.alexanov@gmail.com>
2018-02-16 15:20:46 +0300

    datasource.get promise-based optimization

--- src/component/workflow/fetch.ts
+++ src/component/workflow/fetch.ts
@@ -14,25 +14,33 @@ export default class Fetch {
     return Promise.all(result).then(() => workflow);
   }
 
+  static success(result: any, direction: Direction, workflow: Workflow) {
+    workflow.log(`resolved ${result.length} items ` +
+      `(index = ${workflow.fetch[direction].startIndex}, count = ${workflow.settings.bufferSize})`);
+    workflow.fetch[direction].newItemsData = result;
+    workflow.fetchCount++;
+  }
+
   static fetchByDirection(direction: Direction, workflow: Workflow): Promise<any> {
+    const _get = <Function>workflow.datasource.get;
+    let _getResult;
+    if (_get.length === 2) { // not DatasourceGetCallback
+      _getResult = _get(workflow.fetch[direction].startIndex, workflow.settings.bufferSize);
+      if (_getResult && typeof _getResult.then === 'function') { // DatasourceGetPromise
+        return _getResult.then(result => Fetch.success(result, direction, workflow));
+      }
+    }
     return new Promise((resolve, reject) => {
       const success = (result) => {
-        workflow.log(`resolved ${result.length} items ` +
-          `(index = ${workflow.fetch[direction].startIndex}, count = ${workflow.settings.bufferSize})`);
-        workflow.fetch[direction].newItemsData = result;
-        workflow.fetchCount++;
+        Fetch.success(result, direction, workflow);
         resolve(true);
       };
-
       const _get = <Function>workflow.datasource.get;
       if (_get.length > 2) { // DatasourceGetCallback
         _get(workflow.fetch[direction].startIndex, workflow.settings.bufferSize, success, reject);
       } else {
-        const _getResult = _get(workflow.fetch[direction].startIndex, workflow.settings.bufferSize);
         if (_getResult && typeof _getResult.subscribe === 'function') { // DatasourceGetObservable
           _getResult.subscribe(success, reject);
-        } else if (_getResult && typeof _getResult.then === 'function') { // DatasourceGetPromise
-          _getResult.then(success, reject);
         } else {
           throw new Error('Datasource.get implementation error');
         }

001d37bc31d567b595cf160cb4a252c199c1abcd
NA <d.alexanov@gmail.com>
2018-03-02 22:16:15 +0300

    finalize mass + bouncing scrolls specs

--- tests/scroll-basic.spec.ts
+++ tests/scroll-basic.spec.ts
@@ -28,7 +28,6 @@ const massForwardScrollsConfigList =
   singleForwardMaxScrollConfigList.map((config, index) => ({
     ...config,
     custom: {
-      ...config.custom,
       direction: Direction.backward,
       count: 3 + index // 3-5 bwd scroll events per config
     }
@@ -38,19 +37,48 @@ const massBackwardScrollsConfigList =
   massForwardScrollsConfigList.map((config, index) => ({
     ...config,
     custom: {
-      ...config.custom,
       direction: Direction.backward,
       count: 3 + index // 3-5 fwd scroll events per config
     }
   }));
 
-const massTwoDirectionalScrollsConfigList =
+const massBouncingScrollsConfigList_fwd =
   massForwardScrollsConfigList.map((config, index) => ({
     ...config,
     custom: {
-      ...config.custom,
-      isTwoDirectional: true,
-      count: (3 + index) * 2 // 3-5 fwd + 3-5 bwd scroll events per config
+      direction: Direction.forward,
+      count: (3 + index) * 2, // 3-5 (fwd + bwd) scroll events per config
+      bouncing: true
+    }
+  }));
+
+const massBouncingScrollsConfigList_bwd =
+  massForwardScrollsConfigList.map((config, index) => ({
+    ...config,
+    custom: {
+      direction: Direction.backward,
+      count: (3 + index) * 2, // 3-5 (fwd + bwd) scroll events per config
+      bouncing: true
+    }
+  }));
+
+const massTwoDirectionalScrollsConfigList_fwd =
+  massForwardScrollsConfigList.map((config, index) => ({
+    ...config,
+    custom: {
+      direction: Direction.forward,
+      count: (3 + index) * 2, // 3-5 fwd + 3-5 bwd scroll events per config
+      mass: true
+    }
+  }));
+
+const massTwoDirectionalScrollsConfigList_bwd =
+  massForwardScrollsConfigList.map((config, index) => ({
+    ...config,
+    custom: {
+      direction: Direction.backward,
+      count: (3 + index) * 2, // 3-5 fwd + 3-5 bwd scroll events per config
+      mass: true
     }
   }));
 
@@ -93,15 +121,20 @@ const shouldScroll = (config) => (misc) => (done) => {
 
   spyOn(misc.workflowRunner, 'finalize').and.callFake(() => {
     if (misc.workflowRunner.count < count + scrollCount) {
-      if (config.custom.isTwoDirectional) {
-        const _forward = config.custom.direction === Direction.forward;
+      const _forward = config.custom.direction === Direction.forward;
+      if (config.custom.bouncing) {
         config.custom.direction = _forward ? Direction.backward : Direction.forward;
+      } else if (config.custom.mass) {
+        if (misc.workflowRunner.count - count === (scrollCount / 2)) {
+          config.custom.direction = _forward ? Direction.backward : Direction.forward;
+        }
       }
       if (misc.workflowRunner.count === count + scrollCount - 1) {
         result = calculateIt(config, misc);
       }
       doScrollMax(config, misc);
     } else {
+      // expectations
       const direction = config.custom.direction;
       const edgeItem = misc.workflow.buffer.getEdgeVisibleItem(direction);
       expect(misc.padding[direction].getSize()).toEqual(result.sizeToClip);
@@ -154,8 +187,38 @@ describe('Basic Scroll Spec', () => {
     )
   );
 
-  describe('Mass max two-directional scroll events', () =>
-    massTwoDirectionalScrollsConfigList.forEach(config =>
+  describe('Bouncing max two-directional scroll events (fwd started)', () =>
+    massBouncingScrollsConfigList_fwd.forEach(config =>
+      makeTest({
+        config,
+        title: 'should process some bouncing scrolls',
+        it: shouldScroll(config)
+      })
+    )
+  );
+
+  describe('Bouncing max two-directional scroll events (bwd started)', () =>
+    massBouncingScrollsConfigList_bwd.forEach(config =>
+      makeTest({
+        config,
+        title: 'should process some bouncing scrolls',
+        it: shouldScroll(config)
+      })
+    )
+  );
+
+  describe('Mass max two-directional scroll events (fwd started)', () =>
+    massTwoDirectionalScrollsConfigList_fwd.forEach(config =>
+      makeTest({
+        config,
+        title: 'should process some two-directional scrolls',
+        it: shouldScroll(config)
+      })
+    )
+  );
+
+  describe('Mass max two-directional scroll events (bwd started)', () =>
+    massTwoDirectionalScrollsConfigList_bwd.forEach(config =>
       makeTest({
         config,
         title: 'should process some two-directional scrolls',

d835ca8c24f29566df9f9ced75a5571f6e792ad8
dhilt <d.alexanov@gmail.com>
2018-03-18 19:59:01 +0300

    horizontal demo update

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -151,7 +151,7 @@ export class AppComponent {
       for (let i = index; i <= index + count - 1; i++) {
         data.push({ id: i, text: 'item #' + i });
       }
-      setTimeout(() => success(data), 500);
+      success(data);
     },
     settings: {
       horizontal: true

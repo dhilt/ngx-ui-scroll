95ce517ab2b978c44b348a5b514a4fe68fb01609
dhilt <d.alexanov@gmail.com>
2018-04-03 05:07:58 +0300

    fetch processes are single-directional (2)

--- src/component/workflow/fetch.ts
+++ src/component/workflow/fetch.ts
@@ -1,39 +1,39 @@
 import { Workflow } from '../workflow';
-import { Direction } from '../interfaces/index';
 
 export default class Fetch {
 
-  static run(workflow: Workflow): Promise<any> {
-    const result = [];
-    if (workflow.fetch[Direction.backward].shouldFetch) {
-      result.push(Fetch.fetchByDirection(Direction.backward, workflow));
+  static run(workflow: Workflow) {
+    const direction = workflow.direction;
+    if (!workflow.fetch[direction].shouldFetch) {
+      return workflow;
     }
-    if (workflow.fetch[Direction.forward].shouldFetch) {
-      result.push(Fetch.fetchByDirection(Direction.forward, workflow));
-    }
-    return Promise.all(result).then(() => workflow);
+    const result = new Promise((resolve, reject) => {
+      const success = (data) => {
+        Fetch.success(data, workflow);
+        resolve(workflow);
+      };
+      Fetch.get(workflow, success, reject);
+    });
+    return result;
   }
 
-  static success(data: any, direction: Direction, workflow: Workflow) {
+  static success(data: any, workflow: Workflow) {
+    const direction = workflow.direction;
     workflow.log(`resolved ${data.length} ${direction} items ` +
       `(index = ${workflow.fetch[direction].startIndex}, count = ${workflow.settings.bufferSize})`);
     workflow.fetch[direction].newItemsData = data;
   }
 
-  static fetchByDirection(direction: Direction, workflow: Workflow): Promise<any> {
-    return new Promise((resolve, reject) => {
-      const success = (data) => {
-        Fetch.success(data, direction, workflow);
-        resolve(true);
-      };
-      const _get = <Function>workflow.datasource.get;
-      const _getResult = _get(workflow.fetch[direction].startIndex, workflow.settings.bufferSize, success, reject);
-      if (_getResult && typeof _getResult.then === 'function') { // DatasourceGetPromise
-        _getResult.then(success, reject);
-      } else if (_getResult && typeof _getResult.subscribe === 'function') { // DatasourceGetObservable
-        _getResult.subscribe(success, reject);
-      }
-    });
+  static get(workflow: Workflow, success: Function, reject: Function) {
+    const _get = <Function>workflow.datasource.get;
+    const _getResult =
+      _get(workflow.fetch[workflow.direction].startIndex, workflow.settings.bufferSize, success, reject);
+    if (_getResult && typeof _getResult.then === 'function') { // DatasourceGetPromise
+      _getResult.then(success, reject);
+    } else if (_getResult && typeof _getResult.subscribe === 'function') { // DatasourceGetObservable
+      _getResult.subscribe(success, reject);
+    }
+
   }
 
 }

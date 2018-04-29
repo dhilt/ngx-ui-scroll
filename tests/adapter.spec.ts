666513f4c46a19043d993fee737545d57117f1f4
NA <d.alexanov@gmail.com>
2018-04-29 19:46:50 +0300

    Adapter reload spec: param and scroll cases

--- tests/adapter.spec.ts
+++ tests/adapter.spec.ts
@@ -1,24 +1,50 @@
 import { Direction } from '../src/component/interfaces';
 import { makeTest } from './scaffolding/runner';
 
-
 const configList = [{
   datasourceSettings: { startIndex: 100, bufferSize: 5, padding: 0.2 },
-  templateSettings: { viewportHeight: 100 }
+  templateSettings: { viewportHeight: 100 },
+  custom: { startIndex: null, scrollCount: null }
 }, {
   datasourceSettings: { startIndex: -50, bufferSize: 4, padding: 0.49 },
-  templateSettings: { viewportHeight: 70 }
+  templateSettings: { viewportHeight: 70 },
+  custom: { startIndex: null, scrollCount: null }
+}, {
+  datasourceSettings: { startIndex: -33, bufferSize: 5, padding: 0.3, horizontal: true },
+  templateSettings: { viewportWidth: 300, itemWidth: 100, horizontal: true },
+  custom: { startIndex: null, scrollCount: null }
 }];
 
-const shouldReload = (config) => (misc) => (done) => {
-  const _forward = misc.scroller.direction === Direction.forward;
+const indexedConfigList = configList.map((config, i) => ({
+  ...config,
+  custom: {
+    ...config.custom,
+    startIndex: [-10, 1255, 2][i]
+  }
+}));
 
+const scrolledConfigList = configList.map((config, i) => ({
+  ...config,
+  custom: {
+    startIndex: [-10, null, 2][i],
+    scrollCount: [3, 5, 2][i]
+  }
+}));
+
+const shouldReload = (config) => (misc) => (done) => {
   spyOn(misc.workflow, 'finalize').and.callFake(() => {
-    if (misc.workflow.cyclesDone === 1) {
-      misc.datasource.adapter.reload();
-    }
-    if (misc.workflow.cyclesDone === 2) {
-      const firstIndex = config.datasourceSettings.startIndex - config.datasourceSettings.bufferSize;
+    if (misc.workflow.cyclesDone < 1 + config.custom.scrollCount) {
+      misc.scrollMax();
+    } else if (misc.workflow.cyclesDone === 1 + config.custom.scrollCount) {
+      if (config.custom.startIndex !== null) {
+        misc.datasource.adapter.reload(config.custom.startIndex);
+      } else {
+        misc.datasource.adapter.reload();
+      }
+    } else {
+      const startIndex = config.custom.startIndex === null ?
+        config.datasourceSettings.startIndex : config.custom.startIndex;
+      const firstIndex = startIndex - config.datasourceSettings.bufferSize;
       const firstItem = misc.scroller.buffer.getFirstVisibleItem();
       expect(firstItem.$index).toEqual(firstIndex);
       done();
@@ -28,11 +54,31 @@ const shouldReload = (config) => (misc) => (done) => {
 
 describe('Adapter Spec', () => {
 
-    describe('Simple reload', () =>
+    describe('simple reload', () =>
       configList.forEach(config =>
         makeTest({
           config,
-          title: 'should work',
+          title: 'should reload at initial position',
+          it: shouldReload(config)
+        })
+      )
+    );
+
+    describe('reload with parameter', () =>
+      indexedConfigList.forEach(config =>
+        makeTest({
+          config,
+          title: 'should reload at param position',
+          it: shouldReload(config)
+        })
+      )
+    );
+
+    describe('reload after scroll', () =>
+      scrolledConfigList.forEach(config =>
+        makeTest({
+          config,
+          title: 'should reload at proper position',
           it: shouldReload(config)
         })
       )

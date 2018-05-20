69ecc255f3938abef9d25b2d4781f544e1dc0343
dhilt <d.alexanov@gmail.com>
2018-05-21 00:53:32 +0300

    Angular 6

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
 

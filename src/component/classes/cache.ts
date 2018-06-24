88db8f082d5c2c93bd1cfc0809e86f02228ffc34
dhilt <d.alexanov@gmail.com>
2018-06-24 15:09:38 +0300

    min/max indexes and average size

--- src/component/classes/cache.ts
+++ src/component/classes/cache.ts
@@ -4,36 +4,57 @@ export class ItemCache {
   $index: number;
   nodeId: string;
   data: any; // todo: cache data only if it is permitted by settings
-
-  params: ClientRect;
+  size: number;
 
   constructor(item: Item) {
     this.$index = item.$index;
     this.nodeId = item.nodeId;
     this.data = item.data;
-    this.params = item.getParams();
+    this.size = item.size;
   }
 }
 
 export class Cache {
   items: Array<ItemCache>;
+  averageSize: number;
+  minIndex: number;
+  maxIndex: number;
 
   constructor() {
     this.items = [];
+    this.averageSize = 0;
+    this.minIndex = -Infinity;
+    this.maxIndex = Infinity;
+  }
+
+  updateAverageSize(item: ItemCache) {
+    this.averageSize = ((this.items.length - 1) * this.averageSize + item.size) / this.items.length;
   }
 
   add(item: Item) {
     const found = this.items.find(i => i.$index === item.$index);
     if (found) {
       found.data = item.data;
-      found.params = item.getParams();
+      if (found.size !== item.size) {
+        this.updateAverageSize(item);
+        found.size = item.size;
+      }
     } else {
       // todo: do we need the list to be sorted? maybe an object?
       this.items.push(new ItemCache(item));
+      if (this.averageSize !== item.size) {
+        this.updateAverageSize(item);
+      }
+    }
+    if (item.$index < this.minIndex) {
+      this.minIndex = item.$index;
+    }
+    if (item.$index > this.maxIndex) {
+      this.maxIndex = item.$index;
     }
   }
 
   get(index: number): ItemCache | undefined {
-    return this.items.find(i => i.$index === index);
+    return this.items.find((item: ItemCache) => item.$index === index);
   }
 }

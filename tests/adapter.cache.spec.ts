1d3ae3222aea7bf8d053ccb7215a0abea2b2c8e2
dhilt <d.alexanov@gmail.com>
2019-07-15 02:41:59 +0300

    enable/disable cache tests

---
+++ tests/adapter.cache.spec.ts
@@ -0,0 +1,77 @@
+import { makeTest, TestBedConfig } from './scaffolding/runner';
+import { Misc } from './miscellaneous/misc';
+
+const configList = [{
+  datasourceSettings: { startIndex: 100, bufferSize: 5, padding: 0.2, adapter: true },
+  templateSettings: { viewportHeight: 100 }
+}];
+const config = configList[0];
+
+const checkCache = (misc: Misc, value: boolean) =>
+  expect(misc.scroller.buffer.cache.enabled).toEqual(value);
+const isCahceDisabled = (misc: Misc) => checkCache(misc, false);
+const isCahceEnabled = (misc: Misc) => checkCache(misc, true);
+
+describe('Adapter Cache Spec', () => {
+
+  makeTest({
+    config,
+    title: 'should have cache disabled by default',
+    it: (misc: Misc) => (done: Function) => {
+      isCahceDisabled(misc);
+      spyOn(misc.workflow, 'finalize').and.callFake(() => {
+        isCahceDisabled(misc);
+        done();
+      });
+    }
+  });
+
+  makeTest({
+    config,
+    title: 'should enable cache before load',
+    it: (misc: Misc) => (done: Function) => {
+      misc.scroller.datasource.adapter.cache(true);
+      isCahceEnabled(misc);
+      done();
+    }
+  });
+
+  makeTest({
+    config,
+    title: 'should enable and disable cache before load',
+    it: (misc: Misc) => (done: Function) => {
+      misc.scroller.datasource.adapter.cache(true);
+      isCahceEnabled(misc);
+      misc.scroller.datasource.adapter.cache(false);
+      isCahceDisabled(misc);
+      done();
+    }
+  });
+
+  makeTest({
+    config,
+    title: 'should enable cache after load',
+    it: (misc: Misc) => (done: Function) => {
+      spyOn(misc.workflow, 'finalize').and.callFake(() => {
+        misc.scroller.datasource.adapter.cache(true);
+        isCahceEnabled(misc);
+        done();
+      });
+    }
+  });
+
+  makeTest({
+    config,
+    title: 'should enable cache before load and disable it after load',
+    it: (misc: Misc) => (done: Function) => {
+      misc.scroller.datasource.adapter.cache(true);
+      isCahceEnabled(misc);
+      spyOn(misc.workflow, 'finalize').and.callFake(() => {
+        misc.scroller.datasource.adapter.cache(false);
+        isCahceDisabled(misc);
+        done();
+      });
+    }
+  });
+
+});

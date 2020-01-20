c385fdf09eefa5f59308617d294cdd4edf73021d
dhilt <d.alexanov@gmail.com>
2020-01-20 07:13:17 +0300

    issue-123 tests and append process refactoring

--- tests/adapter.check.spec.ts
+++ tests/adapter.check.spec.ts
@@ -79,6 +79,12 @@ const configList: TestBedConfig[] = [
   }
 ];
 
+const moreProcessesConfigList = configList
+  .filter((v, i) => i === 1 || i === 4)
+  .map((v, i) => ({
+    ...v, custom: { ...v.custom, prepend: i % 2  === 0 }
+  }));
+
 const updateDOMElement = (misc: Misc, index: number, size: number) => {
   const element = misc.getElement(index);
   if (element) {
@@ -142,6 +148,12 @@ const shouldCheck = (config: TestBedConfig) => (misc: Misc) => (done: Function) 
   });
 };
 
+const shouldSimulateFetch = (misc: Misc, value: boolean) => {
+  const { fetch } = misc.scroller.state;
+  expect(fetch.simulate).toEqual(value);
+  expect(fetch.isReplace).toEqual(value);
+}
+
 const shouldFetchAfterCheck = (config: TestBedConfig) => (misc: Misc) => (done: Function) =>
   spyOn(misc.workflow, 'finalize').and.callFake(() => {
     const { state, datasource: { adapter } } = misc.scroller;
@@ -149,9 +161,10 @@ const shouldFetchAfterCheck = (config: TestBedConfig) => (misc: Misc) => (done: 
       case 2:
         updateDOMElement(misc, <number>config.datasourceSettings.startIndex, 50);
         adapter.check();
+        shouldSimulateFetch(misc, true);
         break;
       case 3:
-        expect(state.fetch.isReplace).toEqual(false);
+        shouldSimulateFetch(misc, false);
         misc.scrollMax();
         break;
       case 4:
@@ -159,7 +172,7 @@ const shouldFetchAfterCheck = (config: TestBedConfig) => (misc: Misc) => (done: 
     }
   });
 
-const shouldDoubleCheckAfterAppend = (config: TestBedConfig) => (misc: Misc) => (done: Function) => {
+const shouldDoubleCheck = (config: TestBedConfig) => (misc: Misc) => (done: Function) => {
   const { state, datasource: { adapter } } = misc.scroller;
   const { itemSize, startIndex } = config.datasourceSettings;
   spyOn(misc.workflow, 'finalize').and.callFake(() => {
@@ -167,15 +180,23 @@ const shouldDoubleCheckAfterAppend = (config: TestBedConfig) => (misc: Misc) => 
       case 2:
         updateDOMElement(misc, <number>startIndex, 50);
         adapter.check();
+        shouldSimulateFetch(misc, true);
         break;
       case 3:
-        adapter.append({ id: MAX_INDEX + 1, text: 'new item', size: itemSize });
+        shouldSimulateFetch(misc, false);
+        if (config.custom.prepend) {
+          adapter.prepend({ id: MIN_INDEX - 1, text: 'new item', size: itemSize });
+        } else {
+          adapter.append({ id: MAX_INDEX + 1, text: 'new item', size: itemSize });
+        }
         break;
       case 4:
         updateDOMElement(misc, <number>startIndex, itemSize);
         adapter.check();
+        shouldSimulateFetch(misc, true);
         break;
       case 5:
+        shouldSimulateFetch(misc, false);
         done();
     }
   });
@@ -191,7 +212,7 @@ describe('Adapter Check Size Spec', () => {
     })
   );
 
-  configList.filter((c, i) => i%2 !== 0).forEach(config =>
+  moreProcessesConfigList.forEach(config =>
     makeTest({
       config,
       title: 'should fetch after check',
@@ -199,11 +220,11 @@ describe('Adapter Check Size Spec', () => {
     })
   );
 
-  configList.filter((c, i) => i%2 === 0).forEach(config =>
+  moreProcessesConfigList.forEach(config =>
     makeTest({
       config,
-      title: 'should check after check and append',
-      it: shouldDoubleCheckAfterAppend(config)
+      title: `should check after check and ${config.custom.prepend ? 'prepend' : 'append'}`,
+      it: shouldDoubleCheck(config)
     })
   );
 

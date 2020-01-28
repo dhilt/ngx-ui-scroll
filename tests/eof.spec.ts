e5060a08b6aa8dde96a2c1e905643c849760643c
dhilt <d.alexanov@gmail.com>
2020-01-28 08:56:42 +0300

    issue-110 eof spec update

--- tests/eof.spec.ts
+++ tests/eof.spec.ts
@@ -1,5 +1,5 @@
 import { Direction } from '../src/component/interfaces';
-import { makeTest } from './scaffolding/runner';
+import { makeTest, MakeTestConfig } from './scaffolding/runner';
 import { Misc } from './miscellaneous/misc';
 
 const min = 1, max = 100, scrollCount = 10;
@@ -19,6 +19,29 @@ describe('EOF/BOF Spec', () => {
     }
   };
 
+  const initializeBofEofContainer = (misc: Misc) => {
+    const { bof$, eof$ } = misc.datasource.adapter;
+    misc.shared.bofEofContainer = {
+      bof: { count: 0, value: false },
+      eof: { count: 0, value: false }
+    };
+    const { bof, eof } = misc.shared.bofEofContainer;
+    eof.subscription = eof$.subscribe(value => {
+      eof.count++;
+      eof.value = value;
+    });
+    bof.subscription = bof$.subscribe(value => {
+      bof.count++;
+      bof.value = value;
+    });
+  };
+
+  const disposeBofEofContainer = (misc: Misc) => {
+    const { bof, eof } = misc.shared.bofEofContainer;
+    eof.subscription.unsubscribe();
+    bof.subscription.unsubscribe();
+  };
+
   const expectLimit = (misc: Misc, direction: Direction, noscroll = false) => {
     const _forward = direction === Direction.forward;
     const elements = misc.getElements();
@@ -29,12 +52,18 @@ describe('EOF/BOF Spec', () => {
     }
     expect(misc.checkElementId(elements[_forward ? elements.length - 1 : 0], _forward ? max : min)).toEqual(true);
 
-    const { datasource: { adapter: { eof$, bof$ } }, buffer: { eof, bof } } = misc.scroller;
+    const { buffer: { eof, bof } } = misc.scroller;
     expect(bof).toEqual(!_forward);
-    expect(bof).toEqual(bof$.getValue());
+    expect(bof).toEqual(misc.shared.bofEofContainer.bof.value);
     expect(eof).toEqual(_forward);
-    expect(eof).toEqual(eof$.getValue());
+    expect(eof).toEqual(misc.shared.bofEofContainer.eof.value);
   };
+
+  const _makeTest = (data: MakeTestConfig) => makeTest({
+    ...data,
+    before: (misc: Misc) => initializeBofEofContainer(misc),
+    after: (misc: Misc) => disposeBofEofContainer(misc)
+  });
 
   const runLimitSuite = (eof = 'bof') => {
 
@@ -48,7 +77,7 @@ describe('EOF/BOF Spec', () => {
       const doScroll = (misc: Misc) => isEOF ? misc.scrollMin() : misc.scrollMax();
       const doScrollOpposite = (misc: Misc) => isEOF ? misc.scrollMax() : misc.scrollMin();
 
-      makeTest({
+      _makeTest({
         config: (<any>config)[eof],
         title: `should get ${eof} on init`,
         it: (misc: Misc) => (done: Function) =>
@@ -58,30 +87,29 @@ describe('EOF/BOF Spec', () => {
           })
       });
 
-      makeTest({
+      _makeTest({
         config: (<any>config)[eof],
         title: `should reset ${eof} after scroll`,
         it: (misc: Misc) => (done: Function) =>
           spyOn(misc.workflow, 'finalize').and.callFake(() => {
             const buffer = <any>misc.scroller.buffer;
             const adapter = <any>misc.scroller.datasource.adapter;
-            const eof$ = `${eof}$`;
-            const _eof$ = `${_eof}$`;
+            const { bofEofContainer } = misc.shared;
             if (misc.workflow.cyclesDone === 1) {
               expect(buffer[eof]).toEqual(true);
-              expect(adapter[eof$].getValue()).toEqual(true);
+              expect(bofEofContainer[eof].value).toEqual(true);
               doScroll(misc);
             } else {
               expect(buffer[eof]).toEqual(false);
-              expect(adapter[eof$].getValue()).toEqual(false);
+              expect(bofEofContainer[eof].value).toEqual(false);
               expect(buffer[_eof]).toEqual(false);
-              expect(adapter[_eof$].getValue()).toEqual(false);
+              expect(bofEofContainer[_eof].value).toEqual(false);
               done();
             }
           })
       });
 
-      makeTest({
+      _makeTest({
         config: (<any>config)[eof],
         title: `should stop when ${eof} is reached again`,
         it: (misc: Misc) => (done: Function) =>
@@ -97,7 +125,7 @@ describe('EOF/BOF Spec', () => {
           })
       });
 
-      makeTest({
+      _makeTest({
         config: (<any>config)[eof],
         title: `should reach ${_eof} after some scrolls`,
         it: (misc: Misc) => (done: Function) =>

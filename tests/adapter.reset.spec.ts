bcaa5c964c46c05880e6e1f5394bba00dc889b1c
dhilt <d.alexanov@gmail.com>
2020-03-14 22:34:35 +0300

    issue-139 tests for Adapter.reset with new settings

--- tests/adapter.reset.spec.ts
+++ tests/adapter.reset.spec.ts
@@ -1,17 +1,22 @@
 import { makeTest, TestBedConfig } from './scaffolding/runner';
 import { Misc } from './miscellaneous/misc';
+import { generateDatasourceClass } from './scaffolding/datasources';
 
-const customDefault = { startIndex: null, scrollCount: 0, preLoad: false, interruption: false };
+const customDefault = { settings: null, interruption: false };
+const datasourceName = 'infinite-promise-no-delay';
 
 const configList: TestBedConfig[] = [{
+  datasourceName,
   datasourceSettings: { startIndex: 1, bufferSize: 5, padding: 0.25, adapter: true },
   templateSettings: { viewportHeight: 100 },
   custom: { ...customDefault }
 }, {
+  datasourceName,
   datasourceSettings: { startIndex: -123, bufferSize: 6, padding: 0.62, adapter: true },
-  templateSettings: { viewportHeight: 70 },
+  templateSettings: { viewportHeight: 160 },
   custom: { ...customDefault }
-}, {
+  }, {
+  datasourceName,
   datasourceSettings: { startIndex: -33, bufferSize: 5, padding: 0.3, adapter: true, horizontal: true },
   templateSettings: { viewportWidth: 300, itemWidth: 100, horizontal: true },
   custom: { ...customDefault }
@@ -21,13 +26,26 @@ const interruptionConfigList = configList
   .filter((c, i) => i === 0 || i === 2)
   .map(config => ({
     ...config,
-    custom: { ...config.custom, interruption: true },
+    custom: { ...config.custom, interruption: true }
+  }));
+
+const settingsConfigList = configList
+  .filter((c, i) => i === 0 || i === 2)
+  .map(config => ({
+    ...config,
+    custom: {
+      ...config.custom,
+      settings: {
+        ...config.datasourceSettings,
+        startIndex: 999
+      }
+    },
     // datasourceDevSettings: { debug: true, logProcessRun: true }
   }));
 
 const accessFirstLastVisibleItems = (misc: Misc) => {
   // need to have a pre-call
-  const { firstVisible, lastVisible } = misc.datasource.adapter;
+  const { firstVisible, lastVisible } = misc.workflow.scroller.datasource.adapter;
 };
 
 interface ICheckReset {
@@ -48,15 +66,27 @@ const setCheck = (misc: Misc): ICheckReset => {
 };
 
 const checkReset = (config: TestBedConfig, misc: Misc, oldCheck: ICheckReset) => {
+  const { settings } = config.custom;
   const newCheck = setCheck(misc);
   expect(newCheck.instanceIndex).toEqual(oldCheck.instanceIndex + 1);
-  expect(newCheck.firstVisible).toEqual(oldCheck.firstVisible);
-  expect(newCheck.lastVisible).toEqual(oldCheck.lastVisible);
+  if (!settings) {
+    expect(newCheck.firstVisible).toEqual(oldCheck.firstVisible);
+    expect(newCheck.lastVisible).toEqual(oldCheck.lastVisible);
+  } else {
+    expect(newCheck.firstVisible).toEqual(settings.startIndex);
+    expect(newCheck.lastVisible).toEqual(settings.startIndex + (oldCheck.lastVisible - oldCheck.firstVisible));
+  }
   expect(newCheck.interruptionCount).toEqual(oldCheck.interruptionCount + Number(config.custom.interruption));
 };
 
-const doReset = (misc: Misc) => {
-  misc.datasource.adapter.reset();
+const doReset = (config: TestBedConfig, misc: Misc) => {
+  const { settings } = config.custom;
+  if (!settings) {
+    misc.datasource.adapter.reset();
+  } else {
+    const datasource = new (generateDatasourceClass(datasourceName, settings))();
+    misc.datasource.adapter.reset(datasource);
+  }
   accessFirstLastVisibleItems(misc);
 };
 
@@ -70,9 +100,9 @@ const shouldReset = (config: TestBedConfig) => (misc: Misc) => (done: Function) 
       check = setCheck(misc);
       if (interruption) {
         misc.datasource.adapter.reload();
-        setTimeout(() => doReset(misc));
+        setTimeout(() => doReset(config, misc));
       } else {
-        doReset(misc);
+        doReset(config, misc);
       }
     } else if (!interruption || (interruption && cyclesDone === 3)) {
       checkReset(config, misc, check);
@@ -83,7 +113,7 @@ const shouldReset = (config: TestBedConfig) => (misc: Misc) => (done: Function) 
 
 describe('Adapter Reset Spec', () => {
 
-  describe('rest without params', () =>
+  describe('reset without params', () =>
     configList.forEach(config =>
       makeTest({
         config,
@@ -93,11 +123,21 @@ describe('Adapter Reset Spec', () => {
     )
   );
 
-  describe('rest without params (interrupted)', () =>
+  describe('reset without params (interrupted)', () =>
     interruptionConfigList.forEach(config =>
       makeTest({
         config,
         title: 'should reset with interruption',
+        it: shouldReset(config)
+      })
+    )
+  );
+
+  describe('reset with new settings', () =>
+    settingsConfigList.forEach(config =>
+      makeTest({
+        config,
+        title: 'should reset at new position',
         it: shouldReset(config)
       })
     )

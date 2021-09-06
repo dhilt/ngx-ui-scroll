e070fb6870a3284d32d462662612b96214c5dcf4
Denis Hilt <dhilt.public@gmail.com>
2021-09-06 23:18:03 +0300

    issue-286 Adapter.prepend extra spec, virtual cases

--- tests/adapter.prepend.spec.ts
+++ tests/adapter.prepend.spec.ts
@@ -1,5 +1,6 @@
-import { makeTest, TestBedConfig, ItFuncConfig } from './scaffolding/runner';
-import { generateItem, generateItems } from './miscellaneous/items';
+import { makeTest, TestBedConfig, ItFuncConfig, ItFunc } from './scaffolding/runner';
+import { Data, generateItem, generateItems } from './miscellaneous/items';
+import { DatasourceResetter, getDatasourceClassForReset } from './scaffolding/datasources/class';
 
 const configList: TestBedConfig[] = [{
   datasourceSettings: { startIndex: 100, bufferSize: 4, padding: 0.22, itemSize: 20 },
@@ -18,7 +19,18 @@ const configList: TestBedConfig[] = [{
   templateSettings: { viewportWidth: 300, itemWidth: 75, horizontal: true }
 }];
 
-const configListMany = configList.filter((_, i) => i === 1 || i === 4);
+const configListMany = configList.filter((_, i) => [1, 4].includes(i));
+
+const configListVirtual = configList
+  .filter((_, i) => [0, 3].includes(i))
+  .map(config => ({
+    ...config,
+    datasourceSettings: { ...config.datasourceSettings, minIndex: -40, maxIndex: 40, startIndex: 10 }
+  }))
+  .map(config => ({
+    ...config,
+    datasourceClass: getDatasourceClassForReset({ ...config.datasourceSettings }),
+  }));
 
 const shouldPrepend: ItFuncConfig = () => misc => async done => {
   await misc.relaxNext();
@@ -49,6 +61,49 @@ const shouldPrependMany: ItFuncConfig = () => misc => async done => {
   done();
 };
 
+const shouldPrependVirtual = (
+  config: TestBedConfig, increase: boolean
+): ItFunc => misc => async done => {
+  const { datasource, scroller: { viewport, buffer } } = misc;
+  const amount = 10;
+  const itemSize = misc.getItemSize();
+  const min = config.datasourceSettings.minIndex as number;
+  const max = config.datasourceSettings.maxIndex as number;
+  await misc.relaxNext();
+
+  const minIndex = min - (increase ? 0 : amount);
+  const maxIndex = max + (increase ? amount : 0);
+  const firstId = min - amount;
+  const _firstIndex = buffer.firstIndex;
+  const _lastIndex = buffer.lastIndex;
+  const bwdPadding = viewport.paddings.backward.size;
+  const fwdPadding = viewport.paddings.forward.size;
+  let items: Data[] = [];
+
+  expect(viewport.getScrollableSize()).toEqual((max - min + 1) * itemSize);
+  (datasource as DatasourceResetter).reset(minIndex, maxIndex, firstId);
+  datasource.get(minIndex, amount, data => items = data);
+  await misc.adapter.prepend({ items, bof: true, increase });
+
+  expect(viewport.getScrollableSize()).toEqual((maxIndex - minIndex + 1) * itemSize);
+  expect(viewport.paddings.backward.size).toEqual(bwdPadding + amount * itemSize);
+  expect(viewport.paddings.forward.size).toEqual(fwdPadding);
+  expect(buffer.absMinIndex).toEqual(minIndex);
+  expect(buffer.absMaxIndex).toEqual(maxIndex);
+  expect(buffer.firstIndex).toEqual(_firstIndex + (increase ? amount : 0));
+  expect(buffer.lastIndex).toEqual(_lastIndex + (increase ? amount : 0));
+
+  await misc.scrollToIndexRecursively(maxIndex);
+  await misc.scrollToIndexRecursively(minIndex);
+
+  expect(viewport.getScrollableSize()).toEqual((maxIndex - minIndex + 1) * itemSize);
+  expect(buffer.absMinIndex).toEqual(minIndex);
+  expect(buffer.absMaxIndex).toEqual(maxIndex);
+  expect(buffer.firstIndex).toEqual(minIndex);
+  expect(misc.checkElementContent(minIndex, items[0].id)).toEqual(true);
+  done();
+};
+
 describe('Adapter Prepend Spec', () => {
 
   configList.forEach(config =>
@@ -65,6 +120,16 @@ describe('Adapter Prepend Spec', () => {
       title: 'should prepend many',
       it: shouldPrependMany(config)
     })
+  );
+
+  [true, false].forEach(increase =>
+    configListVirtual.forEach(config =>
+      makeTest({
+        config,
+        title: 'should prepend virtually',
+        it: shouldPrependVirtual(config, increase)
+      })
+    )
   );
 
 });

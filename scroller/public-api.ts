6a878537540f7a1cbe86dd52ef1bc2c74acc17a6
Denis Hilt <dhilt.public@gmail.com>
2021-12-13 01:46:11 +0300

    v3-infrastructure: scroller project configuration

---
+++ scroller/public-api.ts
@@ -0,0 +1,4 @@
+export { UiScrollModule } from './src/ui-scroll.module';
+export { UiScrollDirective } from './src/ui-scroll.directive';
+export { IAdapter, IDatasource, Datasource } from './src/ui-scroll.datasource';
+export { SizeStrategy } from './src/vscroll';

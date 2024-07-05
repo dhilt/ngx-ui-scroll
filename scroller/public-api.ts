b6ab09a3e505977ec1bd1f9547aea24a7011aaa5
arturovt <arthurandrosovich@gmail.com>
2024-07-06 00:08:41 +0300

    fix: export types used by the `Datasource` class
    We need to export these interfaces because they are used by the compiler,
    but they should not be exposed publicly from the module entry point.

--- scroller/public-api.ts
+++ scroller/public-api.ts
@@ -1,5 +1,11 @@
 export { UiScrollModule } from './src/ui-scroll.module';
 export { UiScrollDirective } from './src/ui-scroll.directive';
 export { Datasource } from './src/ui-scroll.datasource';
-export { IAdapter, IDatasource } from './src/types';
+export {
+  RoutinesClassType,
+  IAdapter,
+  IDatasource,
+  IAngularDatasourceParams,
+  IAngularDatasourceConstructed
+} from './src/types';
 export { Routines, SizeStrategy } from './src/vscroll';
